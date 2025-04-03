141ce92bdfceaf4fec3bfd2bb88c7c06509736af
mateszucs96 <szucsmate096@gmail.com>
2025-04-03 22:26:18 +0200

    init starting files for the project

---
+++ task4/src/helpers.ts
@@ -0,0 +1,24 @@
+import { SUPPORTED_COUNTRIES } from './config';
+import { PublicHoliday, PublicHolidayShort } from './types';
+
+const validateCountry = (country: string) => SUPPORTED_COUNTRIES.includes(country);
+
+const validateYear = (year: number) => year === new Date().getFullYear();
+
+export const validateInput = ({ year, country }: { year?: number; country?: string }) => {
+  if (country && !validateCountry(country)) {
+    throw new Error(`Country provided is not supported, received: ${country}`);
+  }
+
+  if (year && !validateYear(year)) {
+    throw new Error(`Year provided not the current, received: ${year}`);
+  }
+
+  return true;
+};
+
+export const shortenPublicHoliday = (holiday: PublicHoliday): PublicHolidayShort => ({
+  name: holiday.name,
+  localName: holiday.localName,
+  date: holiday.date,
+});
